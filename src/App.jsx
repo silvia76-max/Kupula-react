1b4ff73fbf134ba5d890b37858e7e3c3722a3afa
silvia76-max <silviarodriguezexposito.76@gmail.com>
2025-04-09 14:12:14 +0200

    cambio en estilo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Home from './pages/Home'
+import Home from './pages/Home.jsx'
 
 function App() {
   return (
