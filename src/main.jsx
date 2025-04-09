fc18551385e162c103ab12efb7915f53889f9b2e
silvia76-max <silviarodriguezexposito.76@gmail.com>
2025-04-09 14:01:42 +0200

    intro de imagenes y logos

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css' // Estilos globales
 
-// Font Awesome (opción 1 - si instalaste el paquete)
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
